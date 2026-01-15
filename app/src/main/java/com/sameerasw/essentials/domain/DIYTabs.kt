c55c227bbde461c56ea6410c794f59570ff4e692
sameerasw <sameerasw.com@gmail.com>
2026-01-15 15:53:18 +0530

    updated version kaomoji

--- app/src/main/java/com/sameerasw/essentials/domain/DIYTabs.kt
+++ app/src/main/java/com/sameerasw/essentials/domain/DIYTabs.kt
@@ -3,8 +3,8 @@ package com.sameerasw.essentials.domain
 import androidx.annotation.StringRes
 import com.sameerasw.essentials.R
 
-enum class DIYTabs(@StringRes val title: Int, @StringRes val subtitle: Int, val iconRes: Int) {
-    ESSENTIALS(R.string.tab_essentials, R.string.tab_essentials_subtitle, R.drawable.ic_stat_name),
+enum class DIYTabs(@StringRes val title: Int, val subtitle: Any, val iconRes: Int) {
+    ESSENTIALS(R.string.tab_essentials, "(っ◕‿◕)っ", R.drawable.ic_stat_name),
     FREEZE(R.string.tab_freeze, R.string.tab_freeze_subtitle, R.drawable.rounded_mode_cool_24),
     DIY(R.string.tab_diy, R.string.tab_diy_subtitle, R.drawable.rounded_experiment_24)
 }
