3a7bdd15b2e18a3adc1de1d750bc7b12f6841c7b
sameerasw <sameerasw.com@gmail.com>
2026-02-27 14:43:32 +0530

    chore: Minify build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -43,6 +44,11 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
+    }
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
     }
 }
 
