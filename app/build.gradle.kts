4c20adbea68d385ebc1a0d4395aae8002cb39c02
sameerasw <sameerasw.com@gmail.com>
2026-03-05 09:59:19 +0530

    fix: debug build minify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,16 +31,16 @@ android {
 
 //        optimized dev build
 
-          debug {
-             isMinifyEnabled = true
-             isShrinkResources = true
-             isDebuggable = false
-
-             proguardFiles(
-                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                 "proguard-rules.pro"
-             )
-          }
+//          debug {
+//             isMinifyEnabled = true
+//             isShrinkResources = true
+//             isDebuggable = false
+//
+//             proguardFiles(
+//                 getDefaultProguardFile("proguard-android-optimize.txt"),
+//                 "proguard-rules.pro"
+//             )
+//          }
 
         // end
 
