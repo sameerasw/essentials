c5f5ea80def57c1109451036eba9b5f35e9a921e
sameerasw <sameerasw.com@gmail.com>
2026-05-13 18:11:38 +0530

    build: upgrade project jvm target and compatibility to version 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 kotlin {
     compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_11)
+        jvmTarget.set(JvmTarget.JVM_17)
         freeCompilerArgs.add("-Xannotation-default-target=param-property")
         optIn.addAll(
             "androidx.compose.material3.ExperimentalMaterial3Api",
@@ -63,8 +63,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
