76ac0f04f8b9b699db3565d392d2a2cb2740a994
sameerasw <sameerasw.com@gmail.com>
2026-09-04 18:11:59 +0530

    chore: bump app version to 18.0-beta.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,7 +90,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 62
-        versionName = "17.4-beta.2"
+        versionName = "18.0-beta.1"
 
         val whatsNewCounter = 2
         buildConfigField("int", "WHATS_NEW_COUNTER", whatsNewCounter.toString())
